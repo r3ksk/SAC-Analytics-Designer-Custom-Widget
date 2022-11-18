8794a12ceb68520564abd6d39a9fa6537a509272
Raghuraman Ramakrishnan <r3k2020@icloud.com>
2022-11-18 15:06:23 +0800

    fix

--- HelloWorld-Widget/release/0.1.0/helloWorldSACWidget.js
+++ HelloWorld-Widget/release/0.1.0/helloWorldSACWidget.js
@@ -3,11 +3,14 @@
   let tmpl = document.createElement('template');
   let Name;
 
-  tmpl.innerHTML = `<span id="hwName">No Data</span>`;
+  tmpl.innerHTML = `
+  <h1>Hello World</h1>
+  `;
+
   
 
 
-class HelloWorldElement extends HTMLElement {
+  customElements.define("com-sap-HelloWorld", class HelloWorldElement extends HTMLElement {
     
   constructor() {
       // Always call super first in constructor
@@ -18,15 +21,47 @@ class HelloWorldElement extends HTMLElement {
       this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
   }
 
-  set Name (a){
-    this._shadowRoot.getElmentById("hwName").value = a;
+  //Fired when the widget is added to the html DOM of the page
+  connectedCallback(){
+    this._firstConnection = true;
+    this.redraw();
   }
 
-  get Name() {
-    return this._shadowRoot.getElmentById("hwName").value;
+  //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+  disconnectedCallback(){
+
   }
-}
+
+  //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+  onCustomWidgetBeforeUpdate(oChangedProperties) {
+
+  }
+
+  //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+  onCustomWidgetAfterUpdate(oChangedProperties) {
+      if (this._firstConnection){
+          this.redraw();
+      }
+  }
+
+  //When the custom widget is removed from the canvas or the analytic application is closed
+  onCustomWidgetDestroy(){
+  }
+
+  //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+  // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
+  //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+  /*
+  onCustomWidgetResize(width, height){
+
+  }
+  */
+
+  redraw(){
+  }
+
+});
   // Define the new element
-  customElements.define("com-sap-HelloWorld", HelloWorldElement);
+//  customElements.define("com-sap-HelloWorld", HelloWorldElement);
 
 })();  
