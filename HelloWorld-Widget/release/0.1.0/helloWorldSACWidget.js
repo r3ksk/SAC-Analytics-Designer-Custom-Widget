8d00d91af8c66c814f1ff43f4ef9bc0a26d0289a
Raghuraman Ramakrishnan <r3k2020@icloud.com>
2022-11-18 15:31:02 +0800

    fix 2

--- HelloWorld-Widget/release/0.1.0/helloWorldSACWidget.js
+++ HelloWorld-Widget/release/0.1.0/helloWorldSACWidget.js
@@ -1,69 +1,72 @@
-(function () {
+(function()  {
   let tmpl = document.createElement('template');
   tmpl.innerHTML = `
   `;
 
-  customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
+  customElements.define('com-sap-sample-helloworld2', class HelloWorld1 extends HTMLElement {
 
 
-    constructor() {
-      super();
-      this._shadowRoot = this.attachShadow({ mode: "open" });
-      this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-      this._tagContainer;
-      this._tagType = "h1";
-      this._tagText = "Hello World";
-    }
+  constructor() {
+    super(); 
+    this._shadowRoot = this.attachShadow({mode: "open"});
+          this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+          this._firstConnection = false;
+          this._tagContainer;
+          this._tagType = "h1";
+          this._tagText = "Hello World";
+  }
 
-    //Fired when the widget is added to the html DOM of the page
-    connectedCallback() {
-      this._firstConnection = true;
-      this.redraw();
-    }
-
-    //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-    disconnectedCallback() {
-
-    }
-
-    //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-    onCustomWidgetBeforeUpdate(oChangedProperties) {
-
-    }
-
-    //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-    onCustomWidgetAfterUpdate(oChangedProperties) {
-      if (this._firstConnection) {
-        this.redraw();
+      //Fired when the widget is added to the html DOM of the page
+      connectedCallback(){
+          this._firstConnection = true;
+          this.redraw();           
       }
-    }
 
-    //When the custom widget is removed from the canvas or the analytic application is closed
-    onCustomWidgetDestroy() {
-    }
+       //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+      disconnectedCallback(){
+      
+      }
 
+       //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+  onCustomWidgetBeforeUpdate(oChangedProperties) {
 
-    //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-    // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-    //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
-    /*
-    onCustomWidgetResize(width, height){
-        redraw()
-    }
-    */
+  }
 
-    redraw() {
-      if (this._tagText != null){
-        if (this._tagContainer){
-            this._tagContainer.parentNode.removeChild(this._tagContainer);
-        }
+      //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+  onCustomWidgetAfterUpdate(oChangedProperties) {
+          if (this._firstConnection){
+              this.redraw();
+          }
+      }
+      
+      //When the custom widget is removed from the canvas or the analytic application is closed
+      onCustomWidgetDestroy(){
+      
+      }
 
-        var shadow = window.getSelection(this._shadowRoot);
-        this._tagContainer = document.createElement(this._tagType);
-        var theText = document.createTextNode(this._tagText);    
-        this._tagContainer.appendChild(theText); 
-        this._shadowRoot.appendChild(this._tagContainer);
-    }
-    }
+      
+      //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+      // Commented out by default
+      /*
+      onCustomWidgetResize(width, height){
+      
+      }
+      */
+
+      redraw(){
+          if (this._tagContainer){
+              this._tagContainer.parentNode.removeChild(this._tagContainer);
+          }
+
+          var shadow = window.getSelection(this._shadowRoot);
+          this._tagContainer = document.createElement(this._tagType);
+          var theText = document.createTextNode(this._tagText);    
+          this._tagContainer.appendChild(theText); 
+          this._shadowRoot.appendChild(this._tagContainer);
+
+      }
+  
+  
   });
+      
 })();
