c91120015960fa2d7c296903374e7c135c7dc199
Raghuraman Ramakrishnan <r3k2020@icloud.com>
2022-11-18 13:06:03 +0800

    fix

--- HelloWorld-Widget/release/0.1.0/helloWorldWidget.js
+++ HelloWorld-Widget/release/0.1.0/helloWorldWidget.js
@@ -1,21 +1,38 @@
+  // Create a class for the element
 class PopUpInfo extends HTMLElement {
-    
     constructor() {
       // Always call super first in constructor
       super();
-
+  
       // Create a shadow root
       const shadow = this.attachShadow({mode: 'open'});
-
-      let name;
-
-    //   setName (name){
-    //     this.name = name;
+  
+      // Create spans
+      const wrapper = document.createElement('span');
+      wrapper.setAttribute('class', 'wrapper');
+  
+      const icon = document.createElement('span');
+      icon.setAttribute('class', 'icon');
+      icon.setAttribute('tabindex', 0);
+  
+      const info = document.createElement('span');
+      info.setAttribute('class', 'info');
+  
+      // Take attribute content and put it inside the info span
+      const text = this.getAttribute('data-text');
+      info.textContent = text;
+  
+      // Insert icon
+    //   let imgUrl;
+    //   if(this.hasAttribute('img')) {
+    //     imgUrl = this.getAttribute('img');
+    //   } else {
+    //     imgUrl = 'img/default.png';
     //   }
-
-    //   getName (name) {
-    //     return this.name;
-    //   }
+  
+    //   const img = document.createElement('img');
+    //   img.src = imgUrl;
+    //   icon.appendChild(img);
 
       if(this.hasAttribute('name')) {
         this.name = this.getAttribute('name');
@@ -32,6 +49,10 @@ class PopUpInfo extends HTMLElement {
 
       // Take attribute content and put it inside the info span
       hello.textContent = this.name;
+  
+      // Create some CSS to apply to the shadow dom
+      const style = document.createElement('style');
+      console.log(style.isConnected);
   
       style.textContent = `
         .wrapper {
@@ -65,9 +86,14 @@ class PopUpInfo extends HTMLElement {
   
       // Attach the created elements to the shadow dom
       shadow.appendChild(style);
+      console.log(style.isConnected);
+      shadow.appendChild(wrapper);
+      wrapper.appendChild(icon);
+      wrapper.appendChild(info);
       shadow.appendChild(hello);
     }
   }
   
   // Define the new element
   customElements.define('popup-info', PopUpInfo);
+  
