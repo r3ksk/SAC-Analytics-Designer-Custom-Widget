a56609fd294ce86b1b9c338cc1da75778c1542fc
Raghuraman Ramakrishnan <r3k2020@icloud.com>
2022-11-20 13:02:31 +0800

    fix 4_1

--- HelloWorld-Widget/release/0.1.0/helloWorldSACWidget.js
+++ HelloWorld-Widget/release/0.1.0/helloWorldSACWidget.js
@@ -144,7 +144,7 @@
         // End - Getters and Setters
 
         fetchToken() {           
-            let basicAuthCred = "Basic " +  Base64.encode(this._clientID + ":" + this._clientSecret);
+            let basicAuthCred = "Basic " +  atob(this._clientID + ":" + this._clientSecret);
             this.AppendToHeader("Authorization", basicAuthCred);
 
             var requestOptions = {
